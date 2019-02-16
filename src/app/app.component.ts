df2c389bf4156952e17cc069931e45733fe49d4b
Michael Czechowski <mail@dailysh.it>
2019-02-16 17:57:10 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nls-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'spline-generator';
+}
